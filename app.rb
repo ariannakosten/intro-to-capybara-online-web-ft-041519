c2780a5da3e82d8af704d611d71999756f7b5ecd
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-07 17:08:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,7 @@ class Application < Sinatra::Base
     erb :index
   end
   
-  post '/food' do
-  "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
-  end
-end
+  
 
 
 #./app.rb is our main application file, defining the controller that will power this web application
