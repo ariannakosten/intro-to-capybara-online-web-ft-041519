77f0772f1f310d51d0c02e6a66daa171081262ed
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-07 17:08:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ class Application < Sinatra::Base
     erb :index
   end
   
-  
+  post '/greet' do
+    erb :greet
+  end
+end
 
 
 #./app.rb is our main application file, defining the controller that will power this web application
